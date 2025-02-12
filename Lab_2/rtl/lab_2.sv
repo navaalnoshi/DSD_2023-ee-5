0d2f9f04a80cd04ee4b87a72206e1e2c70116e0e
navaalnoshi <navaalnoshi18@gmail.com>
2025-02-12 21:26:11 +0500

    Add files via upload

--- Lab_2/rtl/lab_2.sv
+++ Lab_2/rtl/lab_2.sv
@@ -1,16 +1,18 @@
-
-
-
 module lab_2(
-         input a,
-         input b,
-         input c,
-         output x,
-         output y
-         );
-   assign x= (~(c))^(a|b); //first NOT of c then a OR b then together for both take XOR
-   assign y = (a|b)&(~(a&b)^(a|b)); //  first the not of a AND b , with this then
-   // take XOR with a OR b and finally with the result of this take AND with a OR b
-  
+  input logic a,   
+  input logic b,
+  input logic c,
+  output logic x,  
+  output logic y
+);
+
+  logic or_out, xor_out; 
+
+  assign or_out = a | b;
+  assign x = (~c) ^ or_out;
+  assign xor_out = (~(a & b)) ^ or_out;
+  assign y = or_out & xor_out;
 
 endmodule
+
+
